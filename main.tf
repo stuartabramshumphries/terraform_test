eb8aa7e09f64e04a44acd03e5afe9de7156352e1
Stuart Abrams-Humphries <stuartabramshumphries@gmail.com>
2021-02-04 17:45:35 +0000

    sort security group

--- main.tf
+++ main.tf
@@ -1,28 +1,49 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "eu-west-2"
 }
 
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
+resource "aws_security_group" "web-node" {
+  name = "web-node"
+  description = "Web and SSH sec group"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+    ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }    
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
+
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-098828924dc89ea4a"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.web-node.name]
+  key_name = "tf_lon"
+
   user_data = <<-EOF
            #!/bin/bash
-           echo "Hello, World" > index.html
-           nohup busybox httpd -f -p 8080 &
+           echo "Hello, Stuart" > /etc/motd2
+           yum -y install httpd
+           systemctl enable httpd
+           systemctl start httpd
            EOF
   tags = {
-    Name = "terraform-example"
+    Name = "stuart-terraform-example"
   }
 }
 
